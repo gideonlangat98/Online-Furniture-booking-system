c9093d2bafe261ad87ecce1e1ddf9cee785f9c4c
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-24 00:34:49 +0300

    fetch data from local api

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,17 @@
+//Render One furniture
+
 function renderOneFurniture(furniture) {
   let card = document.createElement('li')
   card.className = 'card'
   card.innerHTML = `
-  <img src = "${furniture.imageUrl}">
+  <img src = "${furniture.imageurl}">
   <div class = 'content'>
-  <h3> $(furniture.name </h3>)
+  <h3> ${furniture.Description} </h3>
 
   <p>
-  $<span class = 'price-value'> ${furniture.price} </span> 50,000 
+  $<span class = 'price-value'> ${furniture.type} </span>
   <p>
-  <p> ${furniture.description} </p>
+  <p> ${furniture.Description} </p>
   </div>
 
   <div class = "button">
@@ -18,8 +20,19 @@ function renderOneFurniture(furniture) {
   </div>
   `
   document.querySelector('#furniture-list').appendChild(card) 
+
+}
+
+function getAllFurniture() {
+  fetch('http://localhost:3000/Furniture')
+  .then(resp => resp.json())
+  .then(furnitureData => furnitureData.forEach(furniture => renderOneFurniture(furniture)))
 }
 
 
+function initialize () {
+  getAllFurniture()
+}
+initialize()
 
 
