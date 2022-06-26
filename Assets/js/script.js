ea542eab6e28068e86ee197aedb9498cde171ef0
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-26 11:15:46 +0300

    style the display of furniture list

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,34 @@
-//style furniture order form
+// const form = document.getElementById("#form-container")
+const add = document.querySelector('.btn')
+
+// //style furniture order form
+// function orderNow() {
+//   document.getElementById("myForm").style.display = "block";
+// }
+
+// function closeForm() {
+//   document.getElementById("myForm").style.display = "none";
+//   alert('Thanks for placing your order!')
+// }
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   alert('Your order has been successfully placed!')
+//   form.request()
+// })
+
+function myOrder() {
+  alert ('THANKS FOR PLACING YOUR FURNITURE ORDER, YOU WILL RECEIVE AN EMAIL CONFIRMATION SHORTLY!')
+}
+
 function orderNow() {
   document.getElementById("myForm").style.display = "block";
 }
 
+
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+  alert ('Thanks for placing your order!')
 }
 
 //Event Listeners
@@ -14,10 +38,10 @@ document.querySelector('#form-container').addEventListener('submit', handleSubmi
 function handleSubmit(e) {
   e.preventDefault()
   let furnitureObj = {
-    imageurl: e.target.imageurl.value,
-    price: e.target.price.value,
-    type: e.target.type.value,
-    location: e.target.location.value
+    Name: e.target.Name.value,
+    phoneNumber: e.target.phoneNumber.value,
+    furnitureName: e.furnitureName.value,
+    Location: e.target.Location.value
   }
   renderOneFurniture(furnitureObj)
 }
@@ -29,7 +53,7 @@ function renderOneFurniture(furniture) {
   card.className = 'card'
   card.innerHTML = `
   <div class="container">
-  <img src = "${furniture.imageurl}" height="250">
+  <img src = "${furniture.imageurl}" height="200">
   <div class = 'content'>
   <h3> ${furniture.type} </h3>
   <p>
@@ -49,6 +73,21 @@ function getAllFurniture() {
   fetch('http://localhost:3000/Furniture')
   .then(resp => resp.json())
   .then(furnitureData => furnitureData.forEach(furniture => renderOneFurniture(furniture)))
+  .catch(err => console.error(err));
+
+}
+
+//POST
+function addToCart(orderForm) {
+  fetch('http://localhost:3000/Furniture', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(orderForm)
+  })
+  .then((resp) => resp.json())
+  .then(furnitureData => console.log(furnitureData))
 }
 
 function initialize () {
